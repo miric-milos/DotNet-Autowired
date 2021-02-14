4aa48a5b56f9781ea9d0ffb9d9172f9216ae07c3
Miloš Mirić <56596844+miric-milos@users.noreply.github.com>
2021-02-14 01:09:40 +0100

    assembled a decent IoC container simulation

--- CustomAutowireApp/IoCContainer/Startup.cs
+++ CustomAutowireApp/IoCContainer/Startup.cs
@@ -4,8 +4,24 @@ using System.Text;
 
 namespace CustomAutowireApp.IoCContainer
 {
+    /// <summary>
+    /// Ioc 
+    /// </summary>
     class Startup
     {
+        private Dictionary<Type, Func<object>> registry = 
+            new Dictionary<Type, Func<object>>();
 
+
+        public void Register<TType, TImpl>() where TImpl : TType, new()
+        {
+            registry.Add(typeof(TType), () => new TImpl());
+        }
+
+        public TType Get<TType>()
+        {
+            var obj = registry[typeof(TType)];
+            return (TType)obj.Invoke();
+        }
     }
 }
--- CustomAutowireApp/Program.cs
+++ CustomAutowireApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CustomAutowireApp.IoCContainer;
+using System;
 
 namespace CustomAutowireApp
 {
@@ -6,7 +7,32 @@ namespace CustomAutowireApp
     {
         static void Main(string[] args)
         {
+            var startup = new Startup();
+            startup.Register<IService, Service>();
 
+            var controller = new Controller(startup.Get<IService>());
+
+            Console.ReadLine();
+        }
+
+        interface IService
+        {
+
+        }
+
+        class Service : IService
+        {
+            public string Field { get; set; } = "Polje";
+        }
+
+        class Controller
+        {
+            private readonly IService service;
+
+            public Controller(IService service)
+            {
+                this.service = service;
+            }
         }
     }
 }
