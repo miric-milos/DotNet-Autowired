5c6b07e4edb22f8c97d55129409fc92dd0e09a39
Miloš Mirić <56596844+miric-milos@users.noreply.github.com>
2021-02-15 15:30:53 +0100

    Created GET<>() method for injecting dependencies

--- CustomAutowireApp/IoCContainer/Startup.cs
+++ CustomAutowireApp/IoCContainer/Startup.cs
@@ -1,27 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Reflection;
+using static CustomAutowireApp.Program;
 
 namespace CustomAutowireApp.IoCContainer
 {
     /// <summary>
-    /// Ioc 
+    /// Ioc container
     /// </summary>
     class Startup
     {
-        private Dictionary<Type, Func<object>> registry = 
+        private static Dictionary<Type, Func<object>> registry =
             new Dictionary<Type, Func<object>>();
 
 
-        public void Register<TType, TImpl>() where TImpl : TType, new()
+        private static void Register<TType, TImpl>() where TImpl : TType, new()
         {
             registry.Add(typeof(TType), () => new TImpl());
         }
 
-        public TType Get<TType>()
+        public static void Init()
         {
-            var obj = registry[typeof(TType)];
-            return (TType)obj.Invoke();
+            Register<IService, Service>();
+        }
+
+        public static T Get<T>() where T : new()
+        {
+            var obj = new T();
+            var fields = obj.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+
+            foreach (var field in fields)
+            {
+                if(registry.ContainsKey(field.FieldType))
+                {
+                    field.SetValue(obj, registry[field.FieldType].Invoke());
+                }
+            }
+
+            return obj;
         }
     }
 }
